abd21270fc1bcf23c269059ba02e35d73fbf6d56
SithilSemitha <sithilsemithad@gmail.com>
2025-12-29 10:16:18 +0530

    fix: Update teh server.js to route the Bok router

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const PORT = 3000;
 app.use(express.json());
 
 const userRoutes = require('./routes/User');
+const bookRoutes = require('./routes/Book');
 
 app.use('/user', userRoutes);
+app.use('/books', bookRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
