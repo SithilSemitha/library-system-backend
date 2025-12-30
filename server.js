83781f006171832c4cfda6817ab96bb9aedb4ce6
SithilSemitha <sithilsemithad@gmail.com>
2025-12-30 17:29:51 +0530

    fix: updated the routes inorder to accept reservations

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.use(express.json());
 const userRoutes = require('./routes/User');
 const bookRoutes = require('./routes/Book');
 const transactionRoutes = require('./routes/transactions');
+const reservationRoutes = require('./routes/reservations');
 
 app.use('/user', userRoutes);
 app.use('/books', bookRoutes);
 app.use('/transactions', transactionRoutes);
-
+app.use('/reservations', reservationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
