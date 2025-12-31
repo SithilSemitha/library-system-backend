3110b8dba29fb7c3af5bf6d91ba11a71cf1f7fd8
SithilSemitha <sithilsemithad@gmail.com>
2025-12-31 09:25:13 +0530

    fix: Updated the routes in the main js file

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const bookRoutes = require('./routes/Book');
 const transactionRoutes = require('./routes/transactions');
 const reservationRoutes = require('./routes/reservations');
 const categoryRoutes = require('./routes/categories');
+const finesRoutes = require('./routes/fines');
 
 app.use('/user', userRoutes);
 app.use('/books', bookRoutes);
 app.use('/transactions', transactionRoutes);
 app.use('/reservations', reservationRoutes);
 app.use('/categories', categoryRoutes);
+app.use('/fines', finesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
