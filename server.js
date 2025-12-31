f4a2171ab5792acf621666e7a659723928ab16d0
SithilSemitha <sithilsemithad@gmail.com>
2025-12-31 09:01:44 +0530

    fix: Updated routes with compatible to categories

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const userRoutes = require('./routes/User');
 const bookRoutes = require('./routes/Book');
 const transactionRoutes = require('./routes/transactions');
 const reservationRoutes = require('./routes/reservations');
+const categoryRoutes = require('./routes/categories');
 
 app.use('/user', userRoutes);
 app.use('/books', bookRoutes);
 app.use('/transactions', transactionRoutes);
 app.use('/reservations', reservationRoutes);
+app.use('/categories', categoryRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
